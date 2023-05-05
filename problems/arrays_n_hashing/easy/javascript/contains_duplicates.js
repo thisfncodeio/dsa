1ec11ecbfc07fbc6aadffac8c6df7e113b18ae3c
Abdul Akibu <aakibu.dev@gmail.com>
2023-05-04 21:40:29 -0400

    adds practice problems for arrays and binary search

--- problems/arrays_n_hashing/easy/javascript/contains_duplicates.js
+++ problems/arrays_n_hashing/easy/javascript/contains_duplicates.js
@@ -61,6 +61,6 @@ var containsDuplicate = (nums) => {
 
 // ===================================================================== //
 
-console.log(contains_duplicate([1,2,3,1])) //=> true
-console.log(contains_duplicate([1,2,3,4])) //=> false
-console.log(contains_duplicate([1,1,1,3,3,4,3,2,4,2])) //=> true
+console.log(containsDuplicate([1,2,3,1])) //=> true
+console.log(containsDuplicate([1,2,3,4])) //=> false
+console.log(containsDuplicate([1,1,1,3,3,4,3,2,4,2])) //=> true
